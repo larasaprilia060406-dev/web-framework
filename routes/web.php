a5f5a5817dcca8c08e131ff1935e5bbac4671541
larasaprilia060406-dev <larasaprilia060406@gmail.com>
2026-03-31 15:24:47 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\DosenController;
+use App\Http\Controllers\Auth\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,35 +10,43 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-// Route biasa
-Route::get('/mahasiswa', function () {
-    $mhs = ['Andi', 'Budi', 'Cici', 'Dedi', 'Eka'];
-    return view('mahasiswa', ['mhs' => $mhs]);
-});
-
-Route::get('/dosen', function () {
-    $dosen = ['Pak Ahmad', 'Bu Betty', 'Pak Charlie', 'Bu Dewi'];
-    return view('dosen', ['dosen' => $dosen]);
-});
-
-// Named Route
-Route::get('/pnp/jurusan/ti/prodi', function () {
-    return view('prodi');
-})->name('prodi');
-
-// Named Route dengan Parameter
-Route::get('/pnp/{jurusan}/{prodi}', function ($jurusan, $prodi) {
-    return view('akademik.prodi', [
-        'jurusan' => $jurusan,
-        'prodi' => $prodi
-    ]);
-})->name('prodi.detail');
-
-// Route untuk home
-Route::get('/home', function () {
-    return view('home');
-});
-
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+
+Route::get('/dosen', [DosenController::class, 'index']);
+
+// Route biasa
+// Route::get('/mahasiswa', function () {
+//     $mhs = ['Andi', 'Budi', 'Cici', 'Dedi', 'Eka'];
+//     return view('mahasiswa', ['mhs' => $mhs]);
+// });
+
+// Route::get('/dosen', function () {
+//     $dosen = ['Pak Ahmad', 'Bu Betty', 'Pak Charlie', 'Bu Dewi'];
+//     return view('dosen', ['dosen' => $dosen]);
+// });
+
+// // Named Route
+// Route::get('/pnp/jurusan/ti/prodi', function () {
+//     return view('prodi');
+// })->name('prodi');
+
+// // Named Route dengan Parameter
+// Route::get('/pnp/{jurusan}/{prodi}', function ($jurusan, $prodi) {
+//     return view('akademik.prodi', [
+//         'jurusan' => $jurusan,
+//         'prodi' => $prodi
+//     ]);
+// })->name('prodi.detail');
+
+// // Route untuk home
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
+// Route::get('/', function () {
+//     return view('home');
+// });
