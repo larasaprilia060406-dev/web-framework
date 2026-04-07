e56b87b464944b2de03be24473a90249a17ff933
larasaprilia060406-dev <larasaprilia060406@gmail.com>
2026-04-07 15:29:31 +0700

    Update MahasiswaController.php

--- app/Http/Controllers/Auth/MahasiswaController.php
+++ app/Http/Controllers/Auth/MahasiswaController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
     public function index()
     {
         return "Ini adalah method index di MahasiswaController";
+    }
+
+    public function insertSql()
+    {
+       $query = DB::insert("INSERT INTO students(nim, nama_lengkap, tempat_lahir, tgl_lahir, email, prodi, alamat, created_at, updated_at) VALUES ('2401093008', 'Laras Aprilia', 'Padang', '2006-04-06', 'larasaprilia060406@gmail.com', 'MI', 'Jl. Tanah Sirah no.12 Padang', now(), now())");
     }
 }
